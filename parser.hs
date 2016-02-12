9003d7d70404fe03415ce45053101195727bddf6
Masaki Waga <masakiwaga@gmail.com>
2016-02-12 22:56:34 +0900

    added eval

--- parser.hs
+++ parser.hs
@@ -11,11 +11,11 @@ data LispVal = Atom String
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input =
     case parse parseExpr "lisp" input of
-      Left err -> "No match: " ++ show err
-      Right _ -> "Found value"
+      Left err -> String $ "No match: " ++ show err
+      Right val -> val
 
 spaces :: Parser ()
 spaces = skipMany1 space
@@ -64,5 +64,51 @@ parseQuoted =
       x <- parseExpr
       return $ List [Atom "quote", x]
 
+showVal :: LispVal -> String
+showVal (String str) = "\"" ++ str ++ "\""
+showVal (Atom name) = name
+showVal (Number num) = show num
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List li) = "(" ++ unwordsList li ++ ")"
+showVal (DottedList hd tl) = "(" ++ unwordsList hd ++ " . " ++ showVal tl ++ ")"
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords.map showVal
+
+instance Show LispVal where show = showVal
+
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
+                                 
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n in 
+                          if null parsed 
+                            then 0
+                            else fst.head $ parsed
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
+
+
 main =
-    readExpr <$> head <$> getArgs >>= putStrLn
+    show <$> eval <$> readExpr <$> head <$> getArgs >>= putStrLn
